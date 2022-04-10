6ea9ca3ab6726eb81b33aa73b2714f16c0a81465
Denis Bashkatov <dend3lion@gmail.com>
2022-04-10 18:08:55 +0300

    Linting fixes

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -3,10 +3,9 @@ import playGame from '../index.js';
 const getRandIntFunc = (min, max) => () => Math.floor(Math.random() * (max - min)) + min;
 
 const checkPrime = (num) => {
-  for (let i = 2, s = Math.sqrt(num); i <= s; i++)
-    if (num % i === 0) return 'no';
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) { if (num % i === 0) return 'no'; }
   return (num > 1) ? 'yes' : 'no';
-}
+};
 
 const primeGame = () => {
   const RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".';
